3b991f9ffa52581cb14006834e44309d73029cc5
felgueres <felgueres@catacaldo.local>
2016-11-27 18:16:52 -0800

    Setting method structure for fforecaster class

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # load-forecast
-Short-term load forecasting 
+Short-term load forecasting
+
+Scope:
+
+Forecast 24hr horizon electrical load at 15-min intervals.
+
+Process overview:
+
+1) Data Preprocessing and considerations
+
+> (9) duplicated timestamps with NaN vals for kwh : dropped.
+
+> 2013-08-17 - communication failure: replaced by period mean given dow/timestamp. Replacement period extended 24 hours to cover complete erroneous values.
+
+> Temperature values - Time aware linear interpolation (polynomial could be considered).
+
+> Given the units of kwh, data is considered to be consumption rather than power - ie. when aggregating to hourly values, was not converted to hourly values.
+
+> methods:
+    erroneous_kwh: Replace erroneous kwh entries (ie. zero) with dow-month mean. Has a tolerance timeframe to tackle erroneous data.
+
+    [Show image of correction]
+
+2) Build forecaster class
+
+> Build model with:
+    - Historical load
+    - Temperature
+
+> Tune model / parameters
+> Weather forecast
+> Extrapolate and forecast
--- src/forecaster.py
+++ src/forecaster.py
@@ -1,6 +1,80 @@
-from sklearn.cross_validation import train_split
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cross_validation import train_test_split
 
 class forecaster(object):
     '''
+    Forecasts load.
+
+    Parameters
+    ----------
+
+    path: str
+        Path to csv file.
+
+    model: str
+        Model type:
+        - ‘RF’ : Random Forest (default)
 
     '''
+
+    def __init__(self, path, model = 'RF'):
+
+        self.df = pd.read_csv(path, index_col=0, parse_dates=True)
+
+        # Create features for specified model.
+
+        if model == 'RF':
+
+            self._featurize_randomforest()
+
+    def _featurize_randomforest(self):
+        '''
+        Build features for random forest.
+        '''
+        pass
+
+    def _features_d(self):
+        '''
+        Create features:
+        - min values for day d
+        - max values for day d
+        '''
+        pass
+
+    def _features_d_minus_1(self):
+        '''
+        Create features:
+        - load of day d-1 at time interval h
+        - morning load peak of d-1
+        - evening load peak of d-1
+        '''
+        pass
+
+    def fit(self):
+        '''
+        Fit specified model.
+        '''
+        pass
+
+    def predict(self):
+        '''
+        Predict in-sample and out-of-sample cases.
+        '''
+        pass
+
+    def plot_in_sample(self):
+        '''
+        Plot actual vs. predicted.
+        '''
+        pass
+
+    def plot_error(self):
+        '''
+        Plot error distribution.
+        '''
+        pass
+        
+if __name__ == '__main__':
+    pass
