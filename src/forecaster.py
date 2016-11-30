60e7e6c8c59458e5c50d228248303d5a273f1ddf
felgueres <felgueres@catacaldo.local>
2016-11-29 20:49:26 -0800

    Working Implemented Cross-Val

--- src/forecaster.py
+++ src/forecaster.py
@@ -23,42 +23,93 @@ class Forecaster(object):
         #Load dataset.
         self.df = pd.read_csv(data, index_col=0, parse_dates=True)
 
-        #Initialize model with tuning parameters.
+        #Init model with tuning parameters.
         self.model = RandomForestRegressor(**kwargs)
 
-        #Set number of splits for cross validation
-        self.splits_cross_val = splits_cross_val
-
-        #set response variable
-        self.y = self.df.pop('kwh')
-
-        #set feature space
-        self.X = self.df.values
-
-    def cross_validation(self):
+    def cross_val(self, training_time = '3W', **kwargs):
 
         '''
-        Create train-validate for cross validation.
+        Cross validation timeseries.
 
-        training_time: str
+        training_time: str, default = '3W'
             Time to train a single prediction task.
             (Eg. To cross validate this model we need:
-                - generate as many partitions as possible given the number of splits
+                - generate partitions of training set given by the training time per task
                 - each task will be trained-tested
                 - compute average MSE for all tasks
 
-        Output
+        Returns
         ------
 
+        MSE: List
+            List with MSE errors from each Random Forest predictor.
+
         '''
-        pass
 
-    def cross_val(self):
+        # Set last day to avoid out-of-bounds indices when training last k fold.
+        last_task_day = self.df.index.date[-1]
+        # Get list of indeces for in-sample validation.
+        task_indeces = self.df.resample(training_time).indices.values()
+        # One day timedelta.
+        one_day = pd.Timedelta('1D')
 
-        tscv = TimeSeriesSplit(n_splits = self.splits_cross_val)
+        # List of error from CV.
+        MSE = []
 
-        for train, test in tscv.split(self.X):
-            print("%s %s" % (train, test))
+        for task in task_indeces:
+
+            # Index main dataframe with task.
+            df_task = self.df.iloc[task]
+
+            #Train timeframes
+            train_start = df_task.index[0]
+            train_end = df_task.index[-1]
+
+            #get last in task to use as proxy for the next day.
+            task_proxy = df_task.index.date[-1]
+            if last_task_day == task_proxy:
+                # In case it is, re-index one day before.
+                train_end = train_end-one_day
+
+            #Test timeframes
+            test_start = train_end + pd.Timedelta('15m')
+            test_end = test_start + one_day
+
+            #Vectorize training-test data.
+            X_train, X_test, y_train, y_test = self.vectorize(train_start, train_end, test_start, test_end)
+
+            #Fit-Predict
+            self.model = RandomForestRegressor(**kwargs)
+            self.model.fit(X_train, y_train)
+            y_predict = self.model.predict(X_test)
+
+            MSE.append(mean_squared_error(y_test, y_predict))
+
+        return MSE
+
+    def vectorize(self, train_start, train_end, test_start, test_end):
+
+        '''
+        Vectorize dataframe for algorithm consumption.
+
+        indices: DateTimeIndex
+            Indices to slice main dataframe.
+
+        Returns
+        ------
+        y_train, X_train, y_test, y_test: numpy array
+
+        '''
+        df_train  = self.df[train_start:train_end]
+        df_test = self.df[test_start:test_end]
+
+        y_train = df_train.pop('kwh')
+        X_train = df_train.values
+
+        y_test = df_test.pop('kwh')
+        X_test = df_test.values
+
+        return X_train, X_test, y_train, y_test
 
 if __name__ == '__main__':
     pass
